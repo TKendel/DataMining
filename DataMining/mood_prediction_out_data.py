44e5a0324af0165f5434d6f5b71f1a9d1a4bd050
TKendel <kendeltoni@gmail.com>
2024-04-16 13:42:03 +0200

    Added steps for grouping, by time freq, filling of NA values and the rounding of th averages and putting in a LSTM algorithm which need to be researched on how it works

--- DataMining/mood_prediction_out_data.py
+++ DataMining/mood_prediction_out_data.py
@@ -3,7 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
+from matplotlib import pyplot
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
 
 #open the out excel file
 data = pd.read_csv('dataset_mood_smartphone.csv')
@@ -11,9 +19,9 @@ df = pd.DataFrame(data)
 
 df["time"] = pd.to_datetime(df["time"])
 
+df['intID'] = df.id.astype('category').cat.codes
 
-pivot_df = df.reset_index().pivot_table(values="value", index=["id", "time"], columns="variable", aggfunc='mean')
-
+pivot_df = df.reset_index().pivot_table(values="value", index=["intID", "time"], columns="variable", aggfunc='mean')
 
 # Split the "time" column into "date" and "time" columns
 pivot_df.reset_index(inplace=True)
@@ -27,9 +35,9 @@ pivot_df["time"] = pd.to_datetime(df["time"])
 pivot_df["hour"] = pivot_df["time"].dt.strftime('%H:00')
 pivot_df["hour"] = pd.Categorical(pivot_df["hour"], categories=sorted(pivot_df["hour"].unique()))
 
+
 variable_list = ["circumplex.arousal", "circumplex.valence", "activity", "screen", "appCat.builtin", "appCat.communication", "appCat.entertainment", "appCat.finance", "appCat.game", "appCat.office", "appCat.other", "appCat.social", "appCat.travel", "appCat.unknown", "appCat.utilities", "appCat.weather"]
 time_variable_list = ["screen", "appCat.builtin", "appCat.communication", "appCat.entertainment", "appCat.finance", "appCat.game", "appCat.office", "appCat.other", "appCat.social", "appCat.travel", "appCat.unknown", "appCat.utilities", "appCat.weather"]
-
 
 
 # # # Data summary ##
@@ -117,44 +125,126 @@ time_variable_list = ["screen", "appCat.builtin", "appCat.communication", "appCa
 #     print(upper_limit)
 #     print(lower_limit)
 
-# # ## REMOVING OULTIERS USING QUANTILES
-# Q1 = pivot_df[variable_list].quantile(0.25)
-# Q3 = pivot_df[variable_list].quantile(0.75)
-# IQR = Q3 - Q1
+# ## REMOVING OULTIERS USING QUANTILES
+Q1 = pivot_df[variable_list].quantile(0.01)
+Q3 = pivot_df[variable_list].quantile(0.99)
+IQR = Q3 - Q1
 
-# df = pivot_df[~((pivot_df[variable_list] < (Q1 - 1.5 * IQR)) | (pivot_df[variable_list] > (Q3 + 1.5 * IQR))).any(axis=1)]
-
-
-## REMOVING OUTLIERS 
-df = pivot_df[~(pivot_df['appCat.builtin'] < 0)]
-
-## LOG times
-for variable in time_variable_list:
-    df[f"{variable}log"] = np.log(df[variable])
-
-pd.plotting.scatter_matrix(df[["mood", "activity", "screenlog", "appCat.builtinlog", "appCat.communicationlog"]])
-plt.show()
+df = pivot_df[~((pivot_df[variable_list] < (Q1 - 1.5 * IQR)) | (pivot_df[variable_list] > (Q3 + 1.5 * IQR))).any(axis=1)]
 
 
-# plt.hist(df['appCat.builtin'], bins=30)
+# ## REMOVING OUTLIERS 
+# df = pivot_df[~(pivot_df['appCat.builtin'] < 0)]
 
-# plt.xlabel('Data Values')
-# plt.ylabel('Frequency')
+# ## LOG times
+# for variable in time_variable_list:
+#     df[f"{variable}log"] = np.log(df[variable])
 
-# file_name = f"\{'appCat.builtin'}.png"
-# plt.savefig(file_name)
-# plt.clf()
+# pd.plotting.scatter_matrix(df[["mood", "activity", "screenlog", "appCat.builtinlog", "appCat.communicationlog"]])
+# plt.show()
 
-
-# ## BOX PLOTS
-# for variable in variable_list:
-#     sns.boxplot(np.log(df[variable]))
-
-#     file_name = f"graphs/boxPlotsNoOutliers/{variable}.png"
-#     plt.savefig(file_name)
-#     plt.clf()
-
+df = df[["time", "circumplex.arousal", "circumplex.valence", "activity", "mood"]].copy()
 
 # ## GROUPING
-# g = df.groupby(pd.Grouper(key='time', freq='D')).mean()
-# print(g.first())
+##TODO Group values based on ID before using the dataframe in predictions
+df = df.groupby(pd.Grouper(key='time', axis=0,  
+                      freq='1d', sort=True)).mean()
+
+# Drop all rows that are completly empty
+df = df.dropna( axis=0, how="all")
+# Fill out nas using interpolation
+df = df.interpolate(method="time")
+
+## Round up values
+df.mood = df.mood.round()
+df['circumplex.valence'] = df['circumplex.valence'].round()
+df['circumplex.arousal'] = df['circumplex.arousal'].round()
+# df.to_excel("outputAll.xlsx")
+
+
+
+# convert series to supervised learning
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = pd.DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = pd.concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+
+values = df.values
+# integer encode direction
+encoder = LabelEncoder()
+values[:,3] = encoder.fit_transform(values[:,3])
+# ensure all data is float
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = series_to_supervised(scaled, 1, 1)
+print(reframed.head())
+
+...
+# split into train and test sets
+values = reframed.values
+n_train_hours = 100
+train = values[:n_train_hours, :]
+test = values[n_train_hours:, :]
+# split into input and outputs
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+
+# design network
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
+# fit network
+history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+ 
+# make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+# invert scaling for forecast
+inv_yhat = np.concatenate((yhat, test_X[:, 1:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = np.concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# calculate RMSE
+rmse = np.sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
+
+
+# n_steps = 60
+# features = 1
+# # split into samples
+# X_train, y_train = split_sequence(training_set_scaled, n_steps)
