acf93de85c994cd1279bc35220328ee4ba6b4f3f
Kenia López <104690170+KeniaLS@users.noreply.github.com>
2024-04-18 17:45:31 +0200

    CORRECTED mean_mood shifted

--- mean_mood_shifted.py
+++ mean_mood_shifted.py
@@ -16,17 +16,15 @@ import numpy as np
 # data['new'] = s.map(data.groupby(s).mean_mood.mean().shift(-1))
 # print(data.head(50))
 
+file = ['dataset_no_NA_for_back_fill.csv', 'dataset_no_NA_inter_fill.csv']
 
-
-
-data_original = pd.read_csv('dataset_no_NA_new.csv')
+data_original = pd.read_csv('dataset_no_NA_inter_fill.csv')
 data_copy = data_original.copy()
 
 data = data_copy[['id', 'DATE', 'mood']] 
 
 
 grouped_means = data.groupby(['id'])
-
 grouped_means = data.groupby(['DATE'])['mood'].transform('mean')
 data['mean_mood'] = grouped_means
 
@@ -40,9 +38,22 @@ data['mean_mood_shifted'] = s.map(data.groupby(s).mean_mood.mean().shift(-1))
 to_add = data.loc[:, 'mean_mood':'mean_mood_shifted']
 data_copy = pd.concat([data_copy, to_add], axis=1)
 
-data_copy.to_csv('dataset_mood_shifted.csv', index=False)
+
+data_copy.to_csv('dataset_mood_shifted_inter_fill.csv', index=False)
+
 # check if nan values are present
+
+
+
 print(data_copy.isnull().sum())
+
+
+# # MISSIN VALUE IN MEAN_MOOD_SHIFTED
+# print(np.nonzero(pd.isnull(data_copy['mean_mood_shifted']))) 
+# #print last rows between 227734 
+# c = data_copy[['DATE','mean_mood','mean_mood_shifted']][227725:227740]
+# print(c)    
+
 
 
 
