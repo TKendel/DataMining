b864fda3ee3465af3f569ccdc389a84392d01b2d
InigoMora <47795034+InigoMora@users.noreply.github.com>
2024-04-19 16:33:00 +0200

    Dataframes recalculated with future_moods.py
    The data frames include future moods per id

--- DataMining/dataset/dataframe_per_hour_future_moods.py
+++ DataMining/dataset/dataframe_per_hour_future_moods.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 
 def fill_NA(data):
@@ -45,68 +45,94 @@ dataframe["circumplex.arousal"]=arousal["circumplex.arousal"]
 dataframe["circumplex.valence"]=valence["circumplex.valence"]
 dataframe["activity"]=activity["activity"]
 
+
+#Generates a list of average mood per day
+next_mood=data_k.groupby( ['id', pd.Grouper(key='time', freq='24H')] )["mood"].mean().to_frame().reset_index()
+#Shifts all moods one day behind (so they become the mood of the next day)
+next_mood["time"] = next_mood["time"] + pd.Timedelta('-1 day')
+
+
+
+
+
+
+
+#dataframe["next_mood"] = data_k[data_k['id'] == id]
+#print(dataframe.loc[dataframe['time'].dt.date==next_mood["time"]])
+#print("HEEERE")
+
+#dataframe["next_mood"]= next_mood["activity"] if dataframe.loc[dataframe['time'].dt.date==next_mood["activity"]] else pass
+
+
+
+
+dataframe['next_mood'] = ""
+print("iterating for matches in date and id to add next_mood" )
+for  index, row in dataframe.iterrows():
+    #print(row['time'].date() )
+    for index_, row_ in next_mood.iterrows():
+        #print(row_["time"].date() )
+        if(row_["time"].date()==row['time'].date())and(row_["id"]==row['id']):
+            print("YESSSS")
+            print(row_["time"].date(), row['time'].date(), row_["id"], row['id'] )
+            dataframe['next_mood'][index] = row_["mood"]
+            print(row_["mood"])
+            break   #Break out of the second for-loop, we only need one match, as ther should not be two entries with same ID and Date
+
+
+#dataframe["next_mood"]=next_mood["mood"]
+"""
+dataframe.reset_index(drop=True)
+next_mood.reset_index(drop=True)
+dataframe['next_mood']= np.where((dataframe['time'].dt.date == next_mood['time'].dt.date) & ( dataframe['id'] == next_mood['if']), next_mood['mood'], np.nan)
+"""
+
+#print(len(dataframe))
+#print(dataframe.head())
+
+
+
+
+    #Raw data
 #Print example
 print("Dataframe Head")
 print(dataframe.head())
 print("The dataframe contains", len(dataframe), "data points")
 
-#Save raw results
-print("Saving data to CSV")
-dataframe.to_csv('dataframe_raw_per_hour.csv', index=False)
-print("Data saved succesfully")
 
+    #Interpollated data
 #Interpolate results
 print("Interpolating dataset")
 fwrd_fill, interpolated = fill_NA(dataframe)
-
 #Print example
 print("fwrd_fill Dataframe Head")
 print(fwrd_fill.head())
 print("The dataframe contains", len(fwrd_fill), "data points")
 
-#Save interpolated results
-print("Saving data to CSV")
-fwrd_fill.to_csv('dataframe_fwrd_fill_per_hour.csv', index=False)
-interpolated.to_csv('dataframe_interpolated_per_hour.csv', index=False)
-print("Data saved succesfully")
 
 
-
+    #Drop Raw
     #Remove lines with all NaN in all 'mood', "circumplex.arousal", "circumplex.valence", "activity"
 print("Removing rows with all NaN values (excluding time and id)")
 #'mood', "circumplex.arousal", "circumplex.valence", "activity"
+    #Needs to be copied before sorting or dropna will not work
 drop = dataframe.copy()
 #https://stackoverflow.com/questions/39128856/python-drop-row-if-two-columns-are-nan
 drop.dropna(subset=['mood', "circumplex.arousal", "circumplex.valence", "activity"], how='all', inplace=True)
-
-
 #Print example
 print("Dataframe Head")
 print(drop.head())
 print("The dataframe contains", len(drop), "data points")
 
-#Save raw results
-print("Saving data to CSV")
-drop.to_csv('dataframe_raw_drop_per_hour.csv', index=False)
-print("Data saved succesfully")
 
+    #Drop Interpollated
 #Interpolate results
 print("Interpolating dataset")
 drop_fwrd_fill, drop_interpolated = fill_NA(drop)
-
 #Print example
 print("drop_fwrd_fill Dataframe Head")
 print(drop_fwrd_fill.head())
 print("The dataframe contains", len(drop_fwrd_fill), "data points")
-
-#Save interpolated results
-print("Saving data to CSV")
-drop_fwrd_fill.to_csv('dataframe_drop_fwrd_fill_per_hour.csv', index=False)
-drop_interpolated.to_csv('dataframe_drop_interpolated_per_hour.csv', index=False)
-print("Data saved succesfully")
-
-
-
 
 print("dataframe raw null values: ", dataframe.isnull().sum())
 print("dataframe fwrd_fill null values: ", fwrd_fill.isnull().sum())
@@ -114,3 +140,26 @@ print("dataframe interpolated null values: ", interpolated.isnull().sum())
 print("dataframe drop null values: ", drop.isnull().sum())
 print("dataframe drop_fwrd_fill null values: ", drop_fwrd_fill.isnull().sum())
 print("dataframe drop_interpolated null values: ", drop_interpolated.isnull().sum())
+
+
+#Save raw results
+print("Saving data to CSV")
+dataframe.sort_values(by=['time'], inplace=True), dataframe.to_csv('dataframe_raw_per_hour.csv', index=False)
+print("Data saved succesfully")
+
+#Save interpolated results
+print("Saving data to CSV")
+fwrd_fill.sort_values(by=['time'], inplace=True), fwrd_fill.to_csv('dataframe_fwrd_fill_per_hour.csv', index=False)
+interpolated.sort_values(by=['time'], inplace=True), interpolated.to_csv('dataframe_interpolated_per_hour.csv', index=False)
+print("Data saved succesfully")
+
+#Save raw drop results
+print("Saving data to CSV")
+drop.sort_values(by=['time'], inplace=True), drop.to_csv('dataframe_raw_drop_per_hour.csv', index=False)
+print("Data saved succesfully")
+
+#Save interpolated results
+print("Saving data to CSV")
+drop_fwrd_fill.sort_values(by=['time'], inplace=True), drop_fwrd_fill.to_csv('dataframe_drop_fwrd_fill_per_hour.csv', index=False)
+drop_interpolated.sort_values(by=['time'], inplace=True), drop_interpolated.to_csv('dataframe_drop_interpolated_per_hour.csv', index=False)
+print("Data saved succesfully")
