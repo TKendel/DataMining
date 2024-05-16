12ec035da9d5bf76b16791ff5048866f1237465a
InigoMora <47795034+InigoMora@users.noreply.github.com>
2024-05-16 12:31:57 +0200

    Add files via upload

--- assignment_02/lightgbm_ranking.py
+++ assignment_02/lightgbm_ranking.py
@@ -40,6 +40,7 @@ zipped_data = zipfile.ZipFile("training_set_VU_DM.zip")
 zipped_data.namelist()
 """
 
+print('importing train dataset')
 data_df = pd.read_csv('training_set_VU_DM.csv')
 
 #Creating target column with clicked and booked boolean columns
@@ -48,22 +49,52 @@ data_df['superscore'] = data_df['superscore'] + data_df['click_bool']
 
 
 
-#https://stackoverflow.com/questions/15723628/pandas-make-a-column-dtype-object-or-factor
-#Selelcting factor featires (categorical)
-categorical = ['site_id', 'visitor_location_country_id', 'prop_country_id', 'prop_id', 'prop_brand_bool', 'promotion_flag', 'srch_destination_id', 'srch_booking_window', 'srch_saturday_night_bool', 'random_bool', 'gross_bookings_usd']
-for factor in categorical:
-     data_df[factor] = data_df[factor].astype('category')
 
 
 #print(list(data_df.columns.values))
 #'srch_id', 'date_time', 'site_id', 'visitor_location_country_id', 'visitor_hist_starrating', 'visitor_hist_adr_usd', 'prop_country_id', 'prop_id', 'prop_starrating', 'prop_review_score', 'prop_brand_bool', 'prop_location_score1', 'prop_location_score2', 'prop_log_historical_price', 'position', 'price_usd', 'promotion_flag', 'srch_destination_id', 'srch_length_of_stay', 'srch_booking_window', 'srch_adults_count', 'srch_children_count', 'srch_room_count', 'srch_saturday_night_bool', 'srch_query_affinity_score', 'orig_destination_distance', 'random_bool', 'comp1_rate', 'comp1_inv', 'comp1_rate_percent_diff', 'comp2_rate', 'comp2_inv', 'comp2_rate_percent_diff', 'comp3_rate', 'comp3_inv', 'comp3_rate_percent_diff', 'comp4_rate', 'comp4_inv', 'comp4_rate_percent_diff', 'comp5_rate', 'comp5_inv', 'comp5_rate_percent_diff', 'comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff', 'comp7_rate', 'comp7_inv', 'comp7_rate_percent_diff', 'comp8_rate', 'comp8_inv', 'comp8_rate_percent_diff', 'click_bool', 'gross_bookings_usd', 'booking_bool', 'superscore'
 #I am guessing  'click_bool' & 'booking_bool' cannot be used for training
 
-#features = ['srch_id', 'date_time', 'site_id', 'visitor_location_country_id', 'visitor_hist_starrating', 'visitor_hist_adr_usd', 'prop_country_id', 'prop_id', 'prop_starrating', 'prop_review_score', 'prop_brand_bool', 'prop_location_score1', 'prop_location_score2', 'prop_log_historical_price', 'position', 'price_usd', 'promotion_flag', 'srch_destination_id', 'srch_length_of_stay', 'srch_booking_window', 'srch_adults_count', 'srch_children_count', 'srch_room_count', 'srch_saturday_night_bool', 'srch_query_affinity_score', 'orig_destination_distance', 'random_bool', 'comp1_rate', 'comp1_inv', 'comp1_rate_percent_diff', 'comp2_rate', 'comp2_inv', 'comp2_rate_percent_diff', 'comp3_rate', 'comp3_inv', 'comp3_rate_percent_diff', 'comp4_rate', 'comp4_inv', 'comp4_rate_percent_diff', 'comp5_rate', 'comp5_inv', 'comp5_rate_percent_diff', 'comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff', 'comp7_rate', 'comp7_inv', 'comp7_rate_percent_diff', 'comp8_rate', 'comp8_inv', 'comp8_rate_percent_diff', 'gross_bookings_usd']
 
-#temp
+
+#NEW Featueres
 #This line creates a new comp values
 data_df['comp']=data_df['comp1_rate'].fillna(0)+data_df['comp2_rate'].fillna(0)+data_df['comp3_rate'].fillna(0)+data_df['comp4_rate'].fillna(0)+data_df['comp5_rate'].fillna(0)+data_df['comp6_rate'].fillna(0)+data_df['comp7_rate'].fillna(0)+data_df['comp8_rate'].fillna(0)
+#data_df['ratio_booked_vs_clicked'] = data_df.groupby(by=['prop_id']).sum()['booking_bool'] / data_df.groupby(by=['prop_id']).sum()['click_bool']
+#****add feature comparing number of rooms with number of people staying*******
+
+#Apply fuynction to every vvalue of column
+#https://saturncloud.io/blog/how-to-create-a-new-column-based-on-the-value-of-another-column-in-pandas/#:~:text=Once%20we%20have%20had%20our,and%20return%20a%20new%20DataFrame.
+def is_positive(number):
+     if number <= 0:
+          return 1
+     return 0
+#Check if input is the same
+def same_country(visitor_location_country_id, prop_country_id):
+     if visitor_location_country_id == prop_country_id:
+          return 1
+     return 0
+#rooms and guests
+data_df['room_minus_guests'] = data_df['srch_room_count']-(data_df['srch_adults_count']+data_df['srch_children_count'])
+data_df['room_per_person'] = data_df['room_minus_guests'].apply(is_positive) #is there a room per person
+data_df['room_guest_ratio'] = (data_df['srch_adults_count']+data_df['srch_children_count'])/ data_df['srch_room_count']#ratio of rooms to guests
+#Normalized price
+data_df['price_usd_norm'] = data_df['price_usd']/ data_df['price_usd'].abs().max()
+#Same country(?)
+#Apply function to two columns --> https://stackoverflow.com/questions/13331698/how-to-apply-a-function-to-two-columns-of-pandas-dataframe
+data_df['same_country'] = data_df.apply(lambda x: same_country(x.visitor_location_country_id, x.prop_country_id), axis=1)
+
+
+
+#https://stackoverflow.com/questions/15723628/pandas-make-a-column-dtype-object-or-factor
+#Selelcting factor featires (categorical)
+categorical = ['site_id', 'visitor_location_country_id', 'prop_country_id', 'prop_id', 'prop_brand_bool', 'promotion_flag', 'srch_destination_id', 'srch_booking_window', 'srch_saturday_night_bool', 'random_bool', 'room_per_person', 'same_country'] #gross_bookings_usd was here before
+for factor in categorical:
+     data_df[factor] = data_df[factor].astype('category')
+
+
+
+
 features = ['srch_id',  'prop_id', 'comp8_rate', 'comp8_inv', 'price_usd', 'prop_log_historical_price', 'prop_review_score', 'prop_starrating', 'comp']  #NOTE:  'gross_bookings_usd' is not on test set
 """
 ##parametters with best ourcome so far*******
@@ -71,20 +102,17 @@ features = ['srch_id',  'prop_id', 'comp8_rate', 'comp8_inv', 'price_usd', 'prop
 features = ['srch_id',  'prop_id', 'comp8_rate', 'comp8_inv', 'price_usd', 'prop_log_historical_price', 'prop_review_score', 'prop_starrating', 'comp']  #NOTE:  'gross_bookings_usd' is not on test set
 """
 
-features = ['srch_id',  'prop_id', 'visitor_location_country_id', 'visitor_hist_starrating', 'visitor_hist_adr_usd', 'prop_country_id', 'prop_starrating', 'prop_review_score', 'prop_brand_bool', 'prop_location_score1', 'prop_location_score2', 'prop_log_historical_price', 'price_usd', 'promotion_flag', 'srch_destination_id', 'srch_length_of_stay', 'srch_booking_window', 'srch_adults_count', 'srch_children_count', 'srch_room_count', 'srch_saturday_night_bool', 'srch_query_affinity_score', 'orig_destination_distance', 'comp1_rate', 'comp1_inv', 'comp1_rate_percent_diff', 'comp2_rate', 'comp2_inv', 'comp2_rate_percent_diff', 'comp3_rate', 'comp3_inv', 'comp3_rate_percent_diff', 'comp4_rate', 'comp4_inv', 'comp4_rate_percent_diff', 'comp5_rate', 'comp5_inv', 'comp5_rate_percent_diff', 'comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff', 'comp7_rate', 'comp7_inv', 'comp7_rate_percent_diff', 'comp8_rate', 'comp8_inv', 'comp8_rate_percent_diff', 'comp']  
+features = ['srch_id',  'prop_id', 'visitor_location_country_id', 'visitor_hist_starrating', 'visitor_hist_adr_usd', 'prop_country_id', 'prop_starrating', 'prop_review_score', 'prop_brand_bool', 'prop_location_score1', 'prop_location_score2', 'prop_log_historical_price', 'price_usd', 'promotion_flag', 'srch_destination_id', 'srch_length_of_stay', 'srch_booking_window', 'srch_adults_count', 'srch_children_count', 'srch_room_count', 'srch_saturday_night_bool', 'srch_query_affinity_score', 'orig_destination_distance', 'comp1_rate', 'comp1_inv', 'comp1_rate_percent_diff', 'comp2_rate', 'comp2_inv', 'comp2_rate_percent_diff', 'comp3_rate', 'comp3_inv', 'comp3_rate_percent_diff', 'comp4_rate', 'comp4_inv', 'comp4_rate_percent_diff', 'comp5_rate', 'comp5_inv', 'comp5_rate_percent_diff', 'comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff', 'comp7_rate', 'comp7_inv', 'comp7_rate_percent_diff', 'comp8_rate', 'comp8_inv', 'comp8_rate_percent_diff', 'comp', 'room_minus_guests', 'room_per_person', 'room_guest_ratio', 'price_usd_norm', 'same_country']  
 
 
-#paper features     - borrowed
-#features = ["srch_id",  'prop_id', 'prop_country_id', 'prop_review_score', 'srch_length_of_stay', 'srch_children_count', 'srch_query_affinity_score', 'comp1_inv', 'comp1_rate_percent_diff', 'comp2_inv', 'comp3_inv', 'comp3_rate_percent_diff', 'comp4_rate', 'comp4_inv', 'comp5_rate_percent_diff', 'comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff']
 
-#****do min max or log on price and other large features****
 
 
 Y = data_df['superscore']
 X = data_df[features]
 
-
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=False)#random_state=1 *****
+#****also try using the entire data set to train********
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.001, shuffle=False)#random_state=1 *****    #test_size=0.2 test_size=0.0001
 
 #define the groups (searches)
 get_group_size = lambda df: df.reset_index().groupby("srch_id")['srch_id'].count()
@@ -92,8 +120,10 @@ train_groups = get_group_size(x_train)
 test_groups = get_group_size(x_test)
 
 print('Preparing model')
-n=100    #n=100
-model = LGBMRanker(objective="lambdarank",n_estimators=n, force_row_wise=True, seed=1) #The model is seeded*****
+n=100   #n=100
+lr=0.1#learning_rate=0.1
+#is_unbalace=True
+model = LGBMRanker(objective="lambdarank",n_estimators=n, force_row_wise=True, seed=1, max_bin=500, learning_rate=lr) #The model is seeded*****   max_bin=255#max_bin=500
 model.fit(x_train,y_train,group=train_groups,eval_set=[(x_test,y_test)],eval_group=[test_groups],eval_metric=['map'])
 
 #getting this error 
@@ -114,7 +144,21 @@ print("RANKED")
 
 print('importing test dataset')
 test_df = pd.read_csv('test_set_VU_DM.csv')
-test_df['comp']=data_df['comp1_rate'].fillna(0)+data_df['comp2_rate'].fillna(0)+data_df['comp3_rate'].fillna(0)+data_df['comp4_rate'].fillna(0)+data_df['comp5_rate'].fillna(0)+data_df['comp6_rate'].fillna(0)+data_df['comp7_rate'].fillna(0)+data_df['comp8_rate'].fillna(0)
+#extra fweatures
+#******This was the previous model, NOTE, it is wrong, as it is using the =data_df['comp1_rate'] instead of the test_df
+#test_df['comp']=data_df['comp1_rate'].fillna(0)+data_df['comp2_rate'].fillna(0)+data_df['comp3_rate'].fillna(0)+data_df['comp4_rate'].fillna(0)+data_df['comp5_rate'].fillna(0)+data_df['comp6_rate'].fillna(0)+data_df['comp7_rate'].fillna(0)+data_df['comp8_rate'].fillna(0)
+test_df['comp']=test_df['comp1_rate'].fillna(0)+test_df['comp2_rate'].fillna(0)+test_df['comp3_rate'].fillna(0)+test_df['comp4_rate'].fillna(0)+test_df['comp5_rate'].fillna(0)+test_df['comp6_rate'].fillna(0)+test_df['comp7_rate'].fillna(0)+test_df['comp8_rate'].fillna(0)
+test_df['room_minus_guests'] = test_df['srch_room_count']-(test_df['srch_adults_count']+test_df['srch_children_count'])
+test_df['room_per_person'] = test_df['room_minus_guests'].apply(is_positive) #is there a room per person
+test_df['room_guest_ratio'] = (test_df['srch_adults_count']+test_df['srch_children_count'])/ test_df['srch_room_count']#ratio of rooms to guests
+test_df['price_usd_norm'] = test_df['price_usd']/ test_df['price_usd'].abs().max()
+test_df['same_country'] = test_df.apply(lambda x: same_country(x.visitor_location_country_id, x.prop_country_id), axis=1)
+
+
+#maybe this is not possible, as the test dataset does not have this information
+#test_df['ratio_booked_vs_clicked'] = test_df.groupby(by=['prop_id']).sum()['booking_bool'] / test_df.groupby(by=['prop_id']).sum()['click_bool']
+
+
 """
 categorical = ['site_id', 'visitor_location_country_id', 'prop_country_id', 'prop_id', 'prop_brand_bool', 'promotion_flag', 'srch_destination_id', 'srch_booking_window', 'srch_saturday_night_bool', 'random_bool', 'gross_bookings_usd']
 """
@@ -149,7 +193,7 @@ ranked = ranked.sort_values(by = ['srch_id', 'score'], ascending = [True, False]
 print("RANKED")
 print(ranked)
 
-print('Savinf to file')
+print('Saving to file')
 out_fearures = ['srch_id',  'prop_id']
 output_df = ranked[out_fearures]
 output_df.to_csv('submission.csv', index=False)
@@ -161,6 +205,11 @@ print(len(output_df))
 
 
 
+print('------------------------------------------------------------ ')
+print('Average nDCG across all queries in the validation at each evaluation point:')
+print('------------------------------------------------------------ ')
+print('Best nDCG@1:', model.best_score_['valid_0']['ndcg@1'])
+print('Best nDCG@5:', model.best_score_['valid_0']['ndcg@5'])
 
 
 """
